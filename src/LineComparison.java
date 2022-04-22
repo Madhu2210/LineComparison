842311b604c1e61f13a246815e1e07f0bd56d524
Madhu Kahar <madhu.kahar2210@gmail.com>
2022-04-22 15:53:28 +0530

    Added: Method added to compare two lines.

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -12,7 +12,7 @@ public class LineComparison {
         double y = Math.pow((y2 - y1), 2);
         Double length = Math.floor(Math.sqrt(x + y));
         System.out.println(length);
-
+        //second length
         System.out.println("Enter the second line of co-ordinated");
         System.out.println("Enter the first (a1,b1) point");
         int a1 = getInputFromUser("first");
@@ -25,12 +25,18 @@ public class LineComparison {
         double length1 = Math.floor(Math.sqrt(a + b));
         System.out.println(length1);
         int result = length.compareTo(length1);
+        if (result == 0) {
+            System.out.println("This line is equals");
+        } else if (result > 0) {
+            System.out.println("First line is grater then second");
+        } else if (result < 0) {
+            System.out.println("Second line is grater then first");
+        }
     }
     public static int getInputFromUser(String number) {
         System.out.println("Enter the " + number + " line co-ordinated");
         Scanner sc = new Scanner(System.in);
         return sc.nextInt();
-
     }
 }
 
